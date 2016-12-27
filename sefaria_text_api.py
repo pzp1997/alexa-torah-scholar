d06d06e748434b52eac7d6a774ece6687c9b6042
Palmer Paul <pzpaul2002@yahoo.com>
2016-12-27 00:42:54 -0500

    make requests to sefaria api

--- sefaria_text_api.py
+++ sefaria_text_api.py
@@ -1,4 +1,14 @@
 import requests
 
+SEFARIA_API_ROOT = 'http://www.sefaria.org/api/'
+TEXT_ENDPOINT = SEFARIA_API_ROOT + 'texts/'
+
+
 def get_verse(book, chapter, verse):
-    return ""
+    text_ref = '.'.join((book, chapter, verse))
+    resp = requests.get(TEXT_ENDPOINT + text_ref,
+                        {'context': 0, 'commentary': 0}).json()
+    return {
+        'text': resp.get('text'),
+        'ref': resp.get('ref')
+    }
