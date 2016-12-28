be910bd2957782cfa196740d7d203174337a9522
Palmer Paul <pzpaul2002@yahoo.com>
2016-12-27 21:30:53 -0500

    better handling of edge cases in sefaria_text_api.get_verse

--- sefaria_text_api.py
+++ sefaria_text_api.py
@@ -1,3 +1,5 @@
+from HTMLParser import HTMLParser
+
 import requests
 
 SEFARIA_API_ROOT = 'http://www.sefaria.org/api/'
@@ -5,10 +7,34 @@ TEXT_ENDPOINT = SEFARIA_API_ROOT + 'texts/'
 
 
 def get_verse(book, chapter, verse):
-    text_ref = '.'.join((book, chapter, verse))
+    text_ref = '.'.join((book.capitalize(), chapter, verse))
     resp = requests.get(TEXT_ENDPOINT + text_ref,
                         {'context': 0, 'commentary': 0}).json()
-    return {
-        'text': resp.get('text'),
-        'ref': resp.get('ref')
-    }
+
+    text = resp.get('text', '')
+    if isinstance(text, (list, tuple)):
+        text = str(text[0]) if len(text) else ''
+
+    text = strip_tags(text).strip()
+    text = ''.join(c for c in text if c.isalnum() or c == ' ')
+
+    return (text.encode('utf-8'), resp.get('ref', '').encode('utf-8'))
+
+
+# Strips HTML tags from a string
+# Created by Stack Overflow user Eloff in his/her answer to the question
+# https://stackoverflow.com/questions/753052/strip-html-from-strings-in-python
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
