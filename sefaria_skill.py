674cbdc52c3b8c018baeeca6bdad51e237b183ef
Palmer Paul <pzpaul2002@yahoo.com>
2017-05-25 18:04:46 -0400

    refactor text intents

--- sefaria_skill.py
+++ sefaria_skill.py
@@ -6,27 +6,40 @@ from sefaria_api import create_ref, get_commentary, get_text
 app = Flask(__name__)
 ask = Ask(app, '/')
 
-@ask.intent('VerseIntent', default={'chapter': '1', 'verse': '1'})
-def handle_verse_intent(book, chapter, verse):
-    app.logger.info('VerseIntent for %s %s:%s', book, chapter, verse)
-    text, ref = sefaria.get_text(book, chapter, verse, verse)
-    app.logger.debug(text)
-    return _build_text_response(text, ref)
 
-
-@ask.intent('ChapterIntent', default={'chapter': '1'})
+@ask.intent('ChapterIntent')
 def handle_chapter_intent(book, chapter):
-    app.logger.info('ChapterIntent for %s %s', book, chapter)
-    text, ref = sefaria.get_text(book, chapter)
+    session.attributes['last_intent'] = 'ChapterIntent'
+    app.logger.info('ChapterIntent: %s %s', book, chapter)
+    return handle_text_request(book, chapter, None, None)
+
+
+@ask.intent('VerseIntent')
+def handle_verse_intent(book, chapter, verse):
+    session.attributes['last_intent'] = 'VerseIntent'
+    app.logger.info('VerseIntent: %s %s %s', book, chapter, verse)
+    return handle_text_request(book, chapter, verse, None)
+
+
+@ask.intent('VerseRangeIntent')
+def handle_verse_range_intent(book, chapter, start_verse, end_verse):
+    session.attributes['last_intent'] = 'VerseRangeIntent'
+    app.logger.info('VerseRangeIntent: %s %s %s %s',
+                    book, chapter, start_verse, end_verse)
+    return handle_text_request(book, chapter, start_verse, end_verse)
+
+
+def handle_text_request(book, chapter, start_verse, end_verse):
+    ref = create_ref(book, chapter, start_verse, end_verse)
+    text, ref = get_text(ref)
     app.logger.debug(text)
+
+    session.attributes['last_ref'] = ref
     return _build_text_response(text, ref)
 
 
-@ask.intent('VerseRangeIntent', default={'chapter': '1', 'start_verse': '1', 'end_verse': '1'})
-def handle_verse_range_intent(book, chapter, start_verse, end_verse):
-    app.logger.info('VerseRangeIntent for %s %s:%s-%s', book, chapter, start_verse, end_verse)
-    text, ref = sefaria.get_text(book, chapter, start_verse, end_verse)
     app.logger.debug(text)
+
     return _build_text_response(text, ref)
 
 
@@ -48,8 +61,7 @@ def _build_text_response(text, ref):
         card_title = 'Sefaria{}{}'.format(' - ' if ref else '', ref)
         return statement(speech_text).simple_card(card_title, card_text)
     else:
-        ref = ref or 'the text'
-        err_msg = render_template('error', ref=ref)
+        err_msg = render_template('error', ref=(ref or 'the text'))
         return statement(err_msg).simple_card('Error', err_msg)
 
 
